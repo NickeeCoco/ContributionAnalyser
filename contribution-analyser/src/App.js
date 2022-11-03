5c7f37a51a8ba5dc864d85f6b512f006c0f79158
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-11-03 09:34:01 -0400

    Data fetch test
    Successfully display GitHub users to the console

--- contribution-analyser/src/App.js
+++ contribution-analyser/src/App.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
-
+import { Octokit } from "octokit"
 
 function App() {
+  const octokit = new Octokit({
+    auth: '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'
+  })
+
+  useEffect(() => {
+    getGithubUsers()
+  }, [])
+
+  async function getGithubUsers() {
+    const result = await octokit.request("GET /users")
+    console.log(result.data)
+  }
+
   return (
     <div className="App">
       <header className="App-header">

eb5eb791b637965ada5696e2e098488b0ffadc81
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-11-03 11:24:13 -0400

    Clean up App component
    Remove fetch functions

--- contribution-analyser/src/App.js
+++ contribution-analyser/src/App.js
@@ -25,38 +25,6 @@ function App() {
     )  
   })
 
-  const octokit = new Octokit({
-    auth: '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'
-  })
-
-  async function getGithubData(owner, repo) {
-    try {
-      const result = await octokit.request("GET /repos/{owner}/{repo}", {
-        owner,
-        repo
-      })
-
-      const {name, description, language, license, stargazers_count, url} = result.data
-
-      setRepoData({name, description, language, license, stargazers_count, url})
-      setIsDataDisplayed(true)
-      setHasError(false)
-    } catch (err) {
-      setHasError(true)
-      setIsDataDisplayed(false)
-    }
-  }
-
-  async function getTopContributors(owner, repo) {
-    const contributors = await octokit.request("GET /repos/{owner}/{repo}/contributors", {
-      owner,
-      repo
-    })
-
-    setContributors(contributors.data.slice(0, 30)) 
-  }
-
-
   return (
     <div className="app">
       <Form 
